8be3e7cb3973cd5e719c809f0e51962c7ddbb713
Jamie Bryson <jamieab98@gmail.com>
2025-11-20 18:27:48 -0500

    Made the form have controlled values. Next step is getting it to fetch post or verifying the state and making sure it's a US state

--- src/components/AddressCard.jsx
+++ src/components/AddressCard.jsx
@@ -1,11 +1,12 @@
 function AddressCard({address}){
 
     return(
-        <p key={address.id}>
+        <div key={address.id}>
+            <div>__________________________</div>
             <div>Place: {address.place}</div>
             <div>Address: {address.address}</div>
             <div>State: {address.state}</div>
-        </p>
+        </div>
     )
 }
 
--- src/components/AddressForm.jsx
+++ src/components/AddressForm.jsx
@@ -8,7 +8,6 @@ function AddressForm(){
 
     function handleSubmit(e){
         e.preventDefault()
-        console.log(location, address, state)
     }
 
     return(
@@ -17,11 +16,11 @@ function AddressForm(){
             <h2>Address From</h2>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="location">Name of Place</label>
-                <input type="text" placeholder="Name..." value={location} id="lcoation" onChange={(e)=>(setLocation(e.target.value))}/>
+                <input type="text" placeholder="Name..." value={location} id="location" onChange={(e)=>(setLocation(e.target.value))}/>
                 <label htmlFor="address">Address</label>
-                <input type="text" placeholder="Address" value={address} id="address" onChange={(e)=>(setAddress(e.target.value))}></input>
+                <input type="text" placeholder="Address" value={address} id="address" autoComplete="address" onChange={(e)=>(setAddress(e.target.value))}/>
                 <label htmlFor="state">State</label>
-                <input type="text" placeholder="State" value={state} id="state" onChange={(e)=>(setState(e.target.value))}></input>
+                <input type="text" placeholder="State" value={state} id="state" onChange={(e)=>(setState(e.target.value))}/>
                 <button type="submit">Submit New Address</button>
             </form>
         </>
