f67d0feb37c5f0fd20ff6b1dd28d261273bc51ec
Jamie Bryson <jamieab98@gmail.com>
2025-11-23 12:08:05 -0500

    Made the login check for the users. If the username and password match, it takes the user to the addresses page

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,25 +1,38 @@
 import { useNavigate } from "react-router-dom"
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function LogIn() {
     const navigate = useNavigate();
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [allLoginCreds, setAllLoginCreds] = useState([])
+
+    useEffect(() => {
+        fetch('https://691f885431e684d7bfc9fffe.mockapi.io/LoginCredientials')
+        .then(response=>response.json())
+        .then(data=>setAllLoginCreds(data))
+        .catch(error=>console.log(error))
+    }, [])
     
     function handleLogin(e){
         e.preventDefault()
-        console.log(username)
-        console.log(password)
+        const filteredCred = allLoginCreds.find((logincreds)=>(logincreds.username.includes(username)))
+        if (filteredCred.username === username && filteredCred.password === password){
+            navigate("/addresses")
+        }
+        else {
+            console.log("try again")
+        }
     }
 
     return(
         <>
             <p>Login Components</p>
             <form onSubmit={handleLogin}>
-                <label>Username:</label>
-                <input type="text" placeholder="username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
-                <label>Password:</label>
-                <input type="text" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                <label htmlFor="username">Username:</label>
+                <input type="text" placeholder="username" value={username} onChange={(e)=>setUsername(e.target.value)} id="username" autoComplete="username"/>
+                <label htmlFor="password">Password:</label>
+                <input type="text" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)} id="password"/>
                 <button type="submit">Log In</button>
             </form>
         </>
