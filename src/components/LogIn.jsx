b0a3bf5ee4c5370ee61f43e1a0170f804e198903
Jamie Bryson <jamieab98@gmail.com>
2025-11-23 10:56:58 -0500

    Made the login form

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,22 +1,27 @@
 import { useNavigate } from "react-router-dom"
+import { useState } from "react";
 
 function LogIn() {
     const navigate = useNavigate();
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
     
-    function handleLogin(){
-        navigate("/addresses")
+    function handleLogin(e){
+        e.preventDefault()
+        console.log(username)
+        console.log(password)
     }
 
     return(
         <>
             <p>Login Components</p>
-            <form>
+            <form onSubmit={handleLogin}>
                 <label>Username:</label>
-                <input type="text" placeholder="username"/>
+                <input type="text" placeholder="username" value={username} onChange={(e)=>setUsername(e.target.value)}/>
                 <label>Password:</label>
-                <input type="text" placeholder="password"/>
+                <input type="text" placeholder="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                <button type="submit">Log In</button>
             </form>
-            <button onClick={handleLogin}>Log In</button>
         </>
     )
 }
